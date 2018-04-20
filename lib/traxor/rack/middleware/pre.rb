077956f30bd5989bf3fb733dd0e7036bc42beb14
Ryan Hansen <ryan.hansen@octanner.com>
2018-04-20 10:51:05 -0600

    Fixing a reporting issue

--- lib/traxor/rack/middleware/pre.rb
+++ lib/traxor/rack/middleware/pre.rb
@@ -19,29 +19,29 @@ module Traxor
             'traxor.rack.middleware.post_middleware_end'
           ]
 
+          tags = {}
+
           if times.all? { |t| env[t].present? }
             pre_time = (env['traxor.rack.middleware.pre_middleware_end'].to_f - env['traxor.rack.middleware.pre_middleware_start'].to_f)
             post_time = (env['traxor.rack.middleware.post_middleware_end'].to_f - env['traxor.rack.middleware.post_middleware_start'].to_f)
             middleware_time = (pre_time + post_time) * 1_000
             total_time = (env['traxor.rack.middleware.post_middleware_end'].to_f - env['traxor.rack.middleware.pre_middleware_start'].to_f) * 1_000
 
-            tags = {}
             if controller
               method = env['REQUEST_METHOD'].to_s
               tags = { controller: Traxor.normalize_name(controller.class), action: Traxor.normalize_name(controller.action_name), method: Traxor.normalize_name(method) }
               controller_path = tags.values.join('.')
 
+              Metric.measure 'rack.request.middleware.duration', "#{middleware_time.round(2)}ms", tags
               Metric.measure "rack.request.middleware.duration.#{controller_path}", "#{middleware_time.round(2)}ms", tags
               Metric.measure "rack.request.duration.#{controller_path}", "#{total_time.round(2)}ms", tags
-              Metric.count 'rack.request.count', 1, tags
               Metric.count "rack.request.count.#{controller_path}", 1, tags
             end
 
-            Metric.measure 'rack.request.middleware.duration', "#{middleware_time.round(2)}ms", tags
             Metric.measure 'rack.request.duration', "#{total_time.round(2)}ms", tags
           end
 
-          Metric.count 'rack.request.count', 1
+          Metric.count 'rack.request.count', 1, tags
 
           [status, headers, body]
         end
--- lib/traxor/version.rb
+++ lib/traxor/version.rb
@@ -1,3 +1,3 @@
 module Traxor
-  VERSION = '0.1.2'.freeze
+  VERSION = '0.1.3'.freeze
 end
