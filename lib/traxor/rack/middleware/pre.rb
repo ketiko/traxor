810a7d13202d8d780473236f8648ec728f08c7c4
Ryan Hansen <ryan.hansen@octanner.com>
2018-04-24 13:45:02 -0600

    fixing again

--- lib/traxor/rack/middleware/pre.rb
+++ lib/traxor/rack/middleware/pre.rb
@@ -11,7 +11,7 @@ module Traxor
           queue_duration = nil
           request_start_ms = env['HTTP_X_REQUEST_START']
           if request_start_ms
-            queue_duration = request_start_ms.to_f - env['traxor.rack.middleware.pre_middleware_start'].to_f
+            queue_duration = env['traxor.rack.middleware.pre_middleware_start'].to_f - request_start_ms.to_f
           end
           status, headers, body = @app.call(env)
           env['traxor.rack.middleware.post_middleware_end'] = Time.now.to_f
