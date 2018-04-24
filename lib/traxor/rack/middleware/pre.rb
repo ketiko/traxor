3e954802096048f9d59d1ed8ac2ac3cf5bc4334e
Ryan Hansen <ryan.hansen@octanner.com>
2018-04-24 15:17:57 -0600

    Fixing bug

--- lib/traxor/rack/middleware/pre.rb
+++ lib/traxor/rack/middleware/pre.rb
@@ -63,9 +63,10 @@ module Traxor
         end
 
         def parse_request_queue(string)
+          value = string.to_s.gsub(/t=/, '')
           DIVISORS.each do |divisor|
             begin
-              t = Time.at(string.to_f / divisor)
+              t = Time.at(value.to_f / divisor)
               return t if t > EARLIEST_ACCEPTABLE_TIME
             rescue RangeError
               # On Ruby versions built with a 32-bit time_t, attempting to
--- lib/traxor/version.rb
+++ lib/traxor/version.rb
@@ -1,3 +1,3 @@
 module Traxor
-  VERSION = '0.1.11'.freeze
+  VERSION = '0.1.12'.freeze
 end
