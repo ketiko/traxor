0f368ac246199d6b82487e36e607ea566afe41ac
Ryan Hansen <ryan.hansen@octanner.com>
2018-05-02 17:21:09 -0600

    fixing more rubocops

--- spec/traxor/sidekiq_spec.rb
+++ spec/traxor/sidekiq_spec.rb
@@ -14,8 +14,12 @@ RSpec.describe Traxor::Sidekiq do
     context 'when an exception' do
       it 'records exception' do
         Thread.new do
-          expect(Traxor::Metric).to receive(:count).with(Traxor::Sidekiq::COUNT_METRIC, 1, tags)
-          expect(Traxor::Metric).to receive(:count).with(Traxor::Sidekiq::EXCEPTION_METRIC, 1, tags)
+          expect(Traxor::Metric).to(
+            receive(:count).with(Traxor::Sidekiq::COUNT_METRIC, 1, tags)
+          )
+          expect(Traxor::Metric).to(
+            receive(:count).with(Traxor::Sidekiq::EXCEPTION_METRIC, 1, tags)
+          )
           begin
             described_class.new.call(MockWorker.new, nil, queue) { raise FakeError }
           rescue FakeError
@@ -29,8 +33,12 @@ RSpec.describe Traxor::Sidekiq do
     context 'when no exception' do
       it 'records metrics' do
         Thread.new do
-          expect(Traxor::Metric).to receive(:measure).with(Traxor::Sidekiq::DURATION_METRIC, any_args, tags)
-          expect(Traxor::Metric).to receive(:count).with(Traxor::Sidekiq::COUNT_METRIC, 1, tags)
+          expect(Traxor::Metric).to(
+            receive(:measure).with(Traxor::Sidekiq::DURATION_METRIC, any_args, tags)
+          )
+          expect(Traxor::Metric).to(
+            receive(:count).with(Traxor::Sidekiq::COUNT_METRIC, 1, tags)
+          )
 
           described_class.new.call(MockWorker.new, nil, queue) { nil }
 
