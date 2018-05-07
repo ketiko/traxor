300df7ba8adf8f5d4b253076f6727dcdd20018f9
Ryan Hansen <ryan.hansen@octanner.com>
2018-05-07 09:28:41 -0600

    Adding more pending

--- spec/traxor/rack/middleware_spec.rb
+++ spec/traxor/rack/middleware_spec.rb
@@ -3,5 +3,49 @@
 require 'traxor/rack/middleware'
 
 RSpec.describe Traxor::Rack::Middleware do
-  pending
+  let(:attributes) do
+    {
+      pre_start_at: nil,
+      pre_finish_at: nil,
+      post_start_at: nil,
+      post_finish_at: nil,
+      request_start_at: nil,
+      gc_stat_before: nil,
+      gc_stat_after: nil
+    }
+  end
+
+  it { is_expected.to have_attributes(attributes) }
+
+  describe '.time_before' do
+    pending
+  end
+
+  describe '.time_after' do
+    pending
+  end
+
+  describe '.middleware_total' do
+    pending
+  end
+
+  describe '.request_queue_total' do
+    pending
+  end
+
+  describe '.gc_count' do
+    pending
+  end
+
+  describe '.gc_major_count' do
+    pending
+  end
+
+  describe '.gc_minor_count' do
+    pending
+  end
+
+  describe '.gc_allocated_objects_count' do
+    pending
+  end
 end
