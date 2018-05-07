dd0df55793712154349020fd8d0fd53569489b10
Ryan Hansen <ryan.hansen@octanner.com>
2018-05-07 09:24:00 -0600

    Adding post middleware specs

--- spec/traxor/rack/middleware/post_spec.rb
+++ spec/traxor/rack/middleware/post_spec.rb
@@ -3,5 +3,23 @@
 require 'traxor/rack/middleware/post'
 
 RSpec.describe Traxor::Rack::Middleware::Post do
-  pending
+  let(:middleware) { described_class.new(app) }
+  let(:app) { instance_double('app', call: nil) }
+  let(:env) { instance_double('env') }
+
+  it 'records the time before the request' do
+    expect { middleware.call(env) }.to(change { Traxor::Rack::Middleware.pre_finish_at })
+  end
+
+  it 'records the time before the request' do
+    expect { middleware.call(env) }.to(change { Traxor::Rack::Middleware.post_start_at })
+  end
+
+  it 'records them in the correct order' do
+    Thread.new do
+      middleware.call(env)
+
+      expect(Traxor::Rack::Middleware.pre_finish_at).to be < Traxor::Rack::Middleware.post_start_at
+    end.join
+  end
 end
